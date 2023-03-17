e4758c79858c1cec093f7fb1057e4a14a712ba9a
pablo-nardi <nardipablo31@gmail.com>
2023-03-17 00:21:18 -0300

    Comenzando proyecto
    - Se creo un solo modelo llamado Auto
    - Se estan haciendo pruebas sobre agregar una nueva columna y lo que implica a nivel de fron
    
    
    Falta agregar un enum, y ver como se adapta, usar el seed y despues probar agregando otra clase a la carpeta models

---
+++ AlquileresAutos/Data/AlquileresAutosContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AlquileresAutos.Models;
+
+namespace AlquileresAutos.Data
+{
+    public class AlquileresAutosContext : DbContext
+    {
+        public AlquileresAutosContext (DbContextOptions<AlquileresAutosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Auto> Autos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Auto>().ToTable("Auto");
+        }
+        //public DbSet<AlquileresAutos.Models.Auto> Auto { get; set; } = default!;
+    }
+}
