1df212bbaa2aec8bb8e4dddc79454bcc446c8848
pablo-nardi <nardipablo31@gmail.com>
2023-03-19 23:22:16 -0300

    Se armo el seed para la clase Auto

---
+++ AlquileresAutos/Data/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using AlquileresAutos.Models;
+
+namespace AlquileresAutos.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AlquileresAutosContext context)
+        {
+            if (context.Autos.Any()){
+                return;
+            }
+
+            var autos = new Auto[]
+            {
+                new Auto{ Patente="AE851AR", capacidadTanque=54, kilometraje=100, fechaCompra=DateTime.Parse("10/07/2021"), detalle="Auto nuevo" },
+                new Auto{ Patente="RC855AR", capacidadTanque=54, kilometraje=1000, fechaCompra=DateTime.Parse("10/07/2020"), detalle="Auto nuevo" }
+            };
+
+            context.Autos.AddRange(autos);
+            context.SaveChanges();
+
+        }
+    }
+}
