a677756a05a1b1d1e4e580ce5826dcaf11cded12
pablo-nardi <nardipablo31@gmail.com>
2023-04-07 21:03:05 -0300

    ABM Modelos

---
+++ AlquileresAutos/Pages/Modelos/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AlquileresAutos.Data;
+using AlquileresAutos.Models;
+
+namespace AlquileresAutos.Pages.Modelos
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AlquileresAutos.Data.AlquileresAutosContext _context;
+
+        public DeleteModel(AlquileresAutos.Data.AlquileresAutosContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Modelo Modelo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Modelos == null)
+            {
+                return NotFound();
+            }
+
+            var modelo = await _context.Modelos.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Modelo = modelo;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Modelos == null)
+            {
+                return NotFound();
+            }
+            var modelo = await _context.Modelos.FindAsync(id);
+
+            if (modelo != null)
+            {
+                Modelo = modelo;
+                _context.Modelos.Remove(Modelo);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ AlquileresAutos/Pages/Modelos/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AlquileresAutos.Data;
+using AlquileresAutos.Models;
+
+namespace AlquileresAutos.Pages.Modelos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AlquileresAutos.Data.AlquileresAutosContext _context;
+
+        public IndexModel(AlquileresAutos.Data.AlquileresAutosContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Modelo> Modelo { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Modelos != null)
+            {
+                Modelo = await _context.Modelos.ToListAsync();
+            }
+        }
+    }
+}
